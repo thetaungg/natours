f09b4c7cd85720edfeff7338e4f50828209840a4
Thet <zarnee96@gmail.com>
2020-01-27 18:28:32 +0630

    added google analytics

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import './App.scss';
 import HomePage from "./pages/home/home.component";
+import ReactGA from 'react-ga';
+
 
 function App() {
+    ReactGA.initialize('UA-157015392-1');
+    ReactGA.pageview(window.location.pathname + window.location.search);
   return (
     <div className="App">
       <HomePage/>
