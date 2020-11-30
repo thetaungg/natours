8280e7b9c71c0b550e321d4780595fe2007b9106
Maxwell <thetaung@nuzay.com>
2020-11-30 19:49:57 +0630

    Updates

---
+++ precache-manifest.04f76479f94326b12e445d6926e0a72b.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a25240f6ece479b92943adaec4362d3",
+    "url": "/natours/index.html"
+  },
+  {
+    "revision": "17edcbff921cbfa2a572",
+    "url": "/natours/static/css/main.f5ee7bab.chunk.css"
+  },
+  {
+    "revision": "fc499e7e76583f3ac354",
+    "url": "/natours/static/js/2.a8542b24.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/natours/static/js/2.a8542b24.chunk.js.LICENSE"
+  },
+  {
+    "revision": "17edcbff921cbfa2a572",
+    "url": "/natours/static/js/main.b1f3722f.chunk.js"
+  },
+  {
+    "revision": "139a945f8351ff0b7a15",
+    "url": "/natours/static/js/runtime-main.77c40394.js"
+  },
+  {
+    "revision": "cd69d3552824dc8e99c3f2953de2735b",
+    "url": "/natours/static/media/hero-small.cd69d355.jpg"
+  },
+  {
+    "revision": "4c42c11f1fc0203483c21f2d6eeb8896",
+    "url": "/natours/static/media/logo-green-1x.4c42c11f.png"
+  },
+  {
+    "revision": "3365abba6477c48e6be892cf6faa8f25",
+    "url": "/natours/static/media/logo-green-2x.3365abba.png"
+  },
+  {
+    "revision": "888043d544c9ed77285afbc360ffd27e",
+    "url": "/natours/static/media/logo-green-small-2x.888043d5.png"
+  },
+  {
+    "revision": "40d4ef82deebd0521e91006d961ead40",
+    "url": "/natours/static/media/logo-white.40d4ef82.png"
+  },
+  {
+    "revision": "7fdf2a886516e020e300ae6bd544ab7d",
+    "url": "/natours/static/media/nat-1-large.7fdf2a88.jpg"
+  },
+  {
+    "revision": "d6c15bf0efcaf4c03c5cb0c9078cdd17",
+    "url": "/natours/static/media/nat-1.d6c15bf0.jpg"
+  },
+  {
+    "revision": "62ed3332960ac5c817fc642538548d7a",
+    "url": "/natours/static/media/nat-10-small.62ed3332.jpg"
+  },
+  {
+    "revision": "2797561ff2a3de15c850f2744b73b6d1",
+    "url": "/natours/static/media/nat-10.2797561f.jpg"
+  },
+  {
+    "revision": "7a25f1999a15760508f5e6131245aeac",
+    "url": "/natours/static/media/nat-2-large.7a25f199.jpg"
+  },
+  {
+    "revision": "4d3783688636b59662283e19991a2912",
+    "url": "/natours/static/media/nat-2.4d378368.jpg"
+  },
+  {
+    "revision": "417d3d741a7a47b3d6ef0307228033a0",
+    "url": "/natours/static/media/nat-3-large.417d3d74.jpg"
+  },
+  {
+    "revision": "cc2b05433cbad0b369d131d02c0cac6a",
+    "url": "/natours/static/media/nat-3.cc2b0543.jpg"
+  },
+  {
+    "revision": "076822ed86e42074e8779d253043fff1",
+    "url": "/natours/static/media/nat-4-small.076822ed.jpg"
+  },
+  {
+    "revision": "61bbf12ae90a30cd432d77087921a02f",
+    "url": "/natours/static/media/nat-4.61bbf12a.jpg"
+  },
+  {
+    "revision": "3847111f31b219cae1e9df82a7941777",
+    "url": "/natours/static/media/nat-5-small.3847111f.jpg"
+  },
+  {
+    "revision": "19f9a22f45a15fbf80d420862bcadffb",
+    "url": "/natours/static/media/nat-5.19f9a22f.jpg"
+  },
+  {
+    "revision": "49bee959dad257d59f8d393a91cb326f",
+    "url": "/natours/static/media/nat-6-small.49bee959.jpg"
+  },
+  {
+    "revision": "1ef6d764fc819e080d40aa72dfe03aa6",
+    "url": "/natours/static/media/nat-6.1ef6d764.jpg"
+  },
+  {
+    "revision": "c75271d776c60c1ce648738f8c7ec8d2",
+    "url": "/natours/static/media/nat-7-small.c75271d7.jpg"
+  },
+  {
+    "revision": "5f846d16e6be9a73e6eb73f620379af2",
+    "url": "/natours/static/media/nat-7.5f846d16.jpg"
+  },
+  {
+    "revision": "5c906a87208c39490964cf34c694250c",
+    "url": "/natours/static/media/nat-8-small.5c906a87.jpg"
+  },
+  {
+    "revision": "14efd82b81787a9764b2cb7eb7afd3e8",
+    "url": "/natours/static/media/nat-8.14efd82b.jpg"
+  },
+  {
+    "revision": "8d747e409de99559df0b098978678b2f",
+    "url": "/natours/static/media/nat-9-small.8d747e40.jpg"
+  },
+  {
+    "revision": "b525a99bcf20d08b2aaf6dd83b7be623",
+    "url": "/natours/static/media/nat-9.b525a99b.jpg"
+  },
+  {
+    "revision": "f556a2c3f37c228f95a5eac3fae8c4a7",
+    "url": "/natours/static/media/video.f556a2c3.webm"
+  },
+  {
+    "revision": "ffcd876c5561db066c09f5922b384473",
+    "url": "/natours/static/media/video.ffcd876c.mp4"
+  }
+]);
